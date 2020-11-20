02879d8ddd424ece386e5577ab1d2437ddef797e
Jonatan <jonataa@net.chalmers.se>
2020-11-20 13:59:25 +0100

    IO, some extra in the Team, new MangeTeam class etc

--- src/ManageTeam.java
+++ src/ManageTeam.java
@@ -1,14 +1,18 @@
 import utilities.Input;
 
-public class ManageTeam {
+import java.util.HashMap;
 
+public class ManageTeam {
+    HashMap<Integer,Team> teamList = new HashMap<>();
     String name= Input.fetchInputString("Type name");
     Team team = new Team(name);
 
     void ManageTeam(){
         this.team= team;
         team.viewTeam();
-
     }
-
+    Team getTeam(Integer id){
+        team = teamList.get(id);
+        return team;
+    }
 }
