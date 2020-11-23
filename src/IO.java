2adb54b1d97c84796fa01f65bb51a2b88c6e41b5
felixfelix <felix@felix.felix>
2020-11-23 16:23:01 +0100

    Added function for saving and loading projects to/from file not implemented yet.

--- src/IO.java
+++ src/IO.java
@@ -2,8 +2,9 @@ import java.io.*;
 
 public class IO{
 
-    public static final File PROJECT_DATA= new File("Files/ProjectData.txt");
+    //public static final File PROJECT_DATA= new File("Files/ProjectData.txt");
     public static final File USER_DATA= new File("Files/UserData.txt");
+    public static final String PROJECT_LOCATION = ("Files/ProjectProjectID.txt");
 
     public static void saveUsers() throws IOException {
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(USER_DATA));
@@ -12,5 +13,15 @@ public class IO{
     public static void readUsers() throws Exception{
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(USER_DATA));
        //users = (ArrayList<User>) ois.readObject();
+    }
+    public static void saveProject(Project project) throws IOException {
+        File saveProjectFile = new File(PROJECT_LOCATION.replace("ProjectID", Integer.toString(project.getID())));
+        //ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(saveProjectFile));
+        //oos.writeObject(project);
+    }
+    public static void loadProject(int projectID) throws IOException {
+        File loadProjectFile = new File(PROJECT_LOCATION.replace("ProjectID", Integer.toString(projectID)));
+        //ObjectInputStream ois = new ObjectInputStream(new FileInputStream(loadProjectFile));
+        //oos.writeObject(project);
     }
 }
