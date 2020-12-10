2b5b6369cb5a6cd7117f5ca6c9c041818556e875
felix <felix@felix.fffff>
2020-12-10 10:18:55 +0100

    css added

--- src/main/java/GradleMain.java
+++ src/main/java/GradleMain.java
@@ -1,12 +1,15 @@
-package sample;
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import view.ChangeScene;
 
-public class Main2 extends Application {
+import java.io.IOException;
+
+public class GradleMain extends Application {
 
     public static void main(String[] args) {
         System.out.println("1");
@@ -15,11 +18,10 @@ public class Main2 extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainMenu.Page.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        //new ChangeScene().changeScene(primaryStage,"Create-Team.Page.fxml");
     }
-
 }
